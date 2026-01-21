ddb607baa9065418d330243945dd3f86d76ca973
Moritz Gluch <moritz-g2@web.de>
2026-01-21 10:57:09 +0100

    STORY-01 Create ViewModel

---
+++ PreciousMetalsManager/ViewModel/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using PreciousMetalsManager.Models;
+
+namespace PreciousMetalsManager.ViewModels
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<MetalHolding> Holdings { get; }
+
+        public MainViewModel()
+        {
+            Holdings = new ObservableCollection<MetalHolding>();
+
+            // Example-Data for In-Memory-CRUD
+            Holdings.Add(new MetalHolding
+            {
+                MetalType = MetalType.Gold,
+                Form = "Bar",
+                Purity = 999.9m,
+                Weight = 100m,
+                Quantity = 1,
+                PurchasePrice = 5800m,
+                PurchaseDate = DateTime.Now,
+                CurrentValue = 6000m,
+                TotalValue = 6000m
+            });
+        }
+    }
+}
