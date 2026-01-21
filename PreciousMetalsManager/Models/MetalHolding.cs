4f60f03b66583bd118ac134ee80d5ed0ed7358f3
Moritz Gluch <moritz-g2@web.de>
2026-01-21 10:55:33 +0100

    STORY-01 Create Model

---
+++ PreciousMetalsManager/Models/MetalHolding.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace PreciousMetalsManager.Models
+{
+    public enum MetalType
+    {
+        Gold,
+        Silver,
+        Bronce,
+        Platinum,
+        Palladium
+    }
+
+    public class MetalHolding
+    {
+        public MetalType MetalType { get; set; }
+
+        public string Form { get; set; } = string.Empty;
+
+        public decimal Purity { get; set; }
+
+        public decimal Weight { get; set; }
+
+        public int Quantity { get; set; }
+
+        public decimal PurchasePrice { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        // Will be calculated later (STORY-03)
+        public decimal CurrentValue { get; set; }
+        public decimal TotalValue { get; set; }
+    }
+}
