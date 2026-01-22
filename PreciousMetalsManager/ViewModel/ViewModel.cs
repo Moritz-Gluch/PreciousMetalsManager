edc74429b82e44971de2f3e013c4a40a0b94d47e
Moritz Gluch <moritz-g2@web.de>
2026-01-22 13:24:21 +0100

    STORY-02 Add and implement filters for metal type and form

--- PreciousMetalsManager/ViewModel/ViewModel.cs
+++ PreciousMetalsManager/ViewModel/ViewModel.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 using PreciousMetalsManager.Models;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace PreciousMetalsManager.ViewModels
 {
-    public class ViewModel
+    public class ViewModel : INotifyPropertyChanged
     {
         public ObservableCollection<MetalHolding> Holdings { get; }
+        public ICollectionView FilteredHoldings { get; }
+
+        private string _formFilter = string.Empty;
+        public string FormFilter
+        {
+            get => _formFilter;
+            set
+            {
+                if (_formFilter != value)
+                {
+                    _formFilter = value;
+                    OnPropertyChanged(nameof(FormFilter));
+                    FilteredHoldings.Refresh();
+                }
+            }
+        }
+
+        private MetalType? _selectedMetalTypeFilter;
+        public MetalType? SelectedMetalTypeFilter
+        {
+            get => _selectedMetalTypeFilter;
+            set
+            {
+                if (_selectedMetalTypeFilter != value)
+                {
+                    _selectedMetalTypeFilter = value;
+                    OnPropertyChanged(nameof(SelectedMetalTypeFilter));
+                    FilteredHoldings.Refresh();
+                }
+            }
+        }
 
         public ViewModel()
         {
@@ -25,6 +58,30 @@ namespace PreciousMetalsManager.ViewModels
                 CurrentValue = 6000m,
                 TotalValue = 6000m
             });
+
+            FilteredHoldings = CollectionViewSource.GetDefaultView(Holdings);
+            FilteredHoldings.Filter = FilterPredicate;
+        }
+
+        private bool FilterPredicate(object obj)
+        {
+            if (obj is not MetalHolding holding)
+                return false;
+
+            if (SelectedMetalTypeFilter != null && holding.MetalType != SelectedMetalTypeFilter)
+                return false;
+
+            if (!string.IsNullOrWhiteSpace(FormFilter) &&
+                !holding.Form.Contains(FormFilter, StringComparison.OrdinalIgnoreCase))
+                return false;
+
+            return true;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
