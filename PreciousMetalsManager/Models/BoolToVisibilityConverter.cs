4a335dfd5374eeaf91f2a830126a667ab9debadc
Moritz Gluch <moritz-g2@web.de>
2026-01-22 13:40:19 +0100

    STORY-02 Add no results message

---
+++ PreciousMetalsManager/Models/BoolToVisibilityConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PreciousMetalsManager.Models
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        public bool Invert { get; set; } = false;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool flag = value is bool b && b;
+            if (Invert)
+                flag = !flag;
+            return flag ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Visibility vis)
+                return vis == Visibility.Visible;
+            return false;
+        }
+    }
+}
