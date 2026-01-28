cb38573c9ba4d4c50ffce9f84ddcbd2e5347f77a
Moritz Gluch <moritz-g2@web.de>
2026-01-28 17:19:51 +0100

    TASK-01 Implement simple statement to test test file

---
+++ tests/MetalHoldingTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PreciousMetalsManager.Models;
+
+namespace PreciousMetalsManager.Tests
+{
+    [TestClass]
+    public class MetalHoldingTest
+    {
+        [TestMethod]
+        public void SimpleTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
