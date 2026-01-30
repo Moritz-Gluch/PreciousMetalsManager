04c456ec0c7fa7cf4489331951578caafdf481fe
Moritz Gluch <moritz-g2@web.de>
2026-01-30 14:15:36 +0100

    FEATURE-07 Create control file for localization and fix files for languages

---
+++ PreciousMetalsManager/Resources/Localization.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace PreciousMetalsManager.Resources
+{
+    /// <summary>
+    /// Provides access to localized strings and allows switching the application language at runtime.
+    /// </summary>
+    public static class Localization
+    {
+        public static string MainWindow_Title => Resources.MainWindow_Title;
+
+        public static string AddButton => Resources.AddButton;
+
+        /// <summary>
+        /// Sets the application language at runtime.
+        /// </summary>
+        /// <param name="cultureName">Culture name, e.g. "de" or "en".</param>
+        public static void SetLanguage(string cultureName)
+        {
+            var culture = new CultureInfo(cultureName);
+            Resources.Culture = culture;
+        }
+    }
+}
