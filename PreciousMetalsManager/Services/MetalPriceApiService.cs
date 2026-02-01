7414d45e5e81d1c0ec085d6c8a383201d90703ce
Moritz Gluch <moritz-g2@web.de>
2026-02-01 16:25:39 +0100

    FEATURE-01 Implement API call

---
+++ PreciousMetalsManager/Services/MetalPriceApiService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace PreciousMetalsManager.Services
+{
+    public class MetalPriceApiService
+    {
+        private static readonly string ApiUrl = "https://api.edelmetalle.de/public.json";
+
+        public async Task<string?> FetchMetalPricesRawAsync()
+        {
+            using var httpClient = new HttpClient();
+            try
+            {
+                var response = await httpClient.GetAsync(ApiUrl);
+                response.EnsureSuccessStatusCode();
+                return await response.Content.ReadAsStringAsync();
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
