73a6d8c8d347dd1a93432de7f8039d6a3156e179
Moritz Gluch <moritz-g2@web.de>
2026-04-04 15:17:13 +0200

    TASK-17 Refactor/Create unit tests for models

--- tests/MetalHoldingTest.cs
+++ tests/MetalHoldingTest.cs
@@ -1,8 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using PreciousMetalsManager.Models;
 using System;
-using System.ComponentModel;
-using System.Windows;
+using System.Collections.Generic;
 
 namespace PreciousMetalsManager.Tests
 {
@@ -45,7 +44,7 @@ namespace PreciousMetalsManager.Tests
         {
             var holding = new MetalHolding();
             string? changedProperty = null;
-            holding.PropertyChanged += (s, e) => changedProperty = e.PropertyName;
+            holding.PropertyChanged += (_, e) => changedProperty = e.PropertyName;
 
             holding.Weight = 42m;
 
@@ -57,10 +56,10 @@ namespace PreciousMetalsManager.Tests
         {
             var holding = new MetalHolding();
             holding.Form = "Barren";
-            bool eventRaised = false;
-            holding.PropertyChanged += (s, e) => eventRaised = true;
+            var eventRaised = false;
+            holding.PropertyChanged += (_, _) => eventRaised = true;
 
-            holding.Form = "Barren"; // Set to same value
+            holding.Form = "Barren";
 
             Assert.IsFalse(eventRaised);
         }
@@ -83,48 +82,89 @@ namespace PreciousMetalsManager.Tests
         }
 
         [TestMethod]
-        public void CollectableType_SetAndGet_WorksCorrectly()
+        public void IsTaxFree_IsTrue_ForHoldingOlderThanOneYear()
         {
-            var holding = new MetalHolding();
-            holding.CollectableType = CollectableType.Numismatic;
-            Assert.AreEqual(CollectableType.Numismatic, holding.CollectableType);
+            var holding = new MetalHolding
+            {
+                PurchaseDate = DateTime.Today.AddYears(-1).AddDays(-1)
+            };
+
+            Assert.IsTrue(holding.IsTaxFree);
         }
 
         [TestMethod]
-        public void CollectableType_PropertyChanged_IsRaised()
+        public void IsTaxFree_IsTrue_OnExactOneYearBoundary()
         {
-            var holding = new MetalHolding();
-            string? changedProperty = null;
-            holding.PropertyChanged += (s, e) => changedProperty = e.PropertyName;
+            var holding = new MetalHolding
+            {
+                PurchaseDate = DateTime.Today.AddYears(-1)
+            };
 
-            holding.CollectableType = CollectableType.SemiNumismatic;
-
-            Assert.AreEqual(nameof(MetalHolding.CollectableType), changedProperty);
+            Assert.IsTrue(holding.IsTaxFree);
         }
 
-        // Not working anymore after refactoring, will be fixed in a future story
-        //[TestMethod]
-        //public void TaxFreeStatus_And_IsTaxFree_AreCorrect_ForOldAndNewHoldings()
-        //{
-        //    var now = DateTime.Today;
-        //    var oneYearAgo = now.AddYears(-1).AddDays(-1);
-        //    var almostOneYearAgo = now.AddYears(-1).AddDays(1);
+        [TestMethod]
+        public void IsTaxFree_IsFalse_ForHoldingNewerThanOneYear()
+        {
+            var holding = new MetalHolding
+            {
+                PurchaseDate = DateTime.Today.AddMonths(-6)
+            };
 
-        //    var holdingOld = new MetalHolding { PurchaseDate = oneYearAgo };
-        //    var holdingNew = new MetalHolding { PurchaseDate = almostOneYearAgo };
+            Assert.IsFalse(holding.IsTaxFree);
+        }
 
-        //    var yesString = Application.Current?.TryFindResource("TaxFreeStatus_Yes") as string;
-        //    var daysString = Application.Current?.TryFindResource("TaxFreeStatus_DaysLeft") as string;
+        [TestMethod]
+        public void IsTaxFree_IsFalse_WhenPurchaseDateIsDefault()
+        {
+            var holding = new MetalHolding();
 
-        //    Assert.IsNotNull(yesString);
-        //    Assert.IsNotNull(daysString);
+            Assert.IsFalse(holding.IsTaxFree);
+        }
 
-        //    Assert.AreEqual(yesString, holdingOld.TaxFreeStatus);
-        //    Assert.IsTrue(holdingOld.IsTaxFree);
+        [TestMethod]
+        public void TaxFreeDaysLeft_ReturnsMaxValue_WhenPurchaseDateIsDefault()
+        {
+            var holding = new MetalHolding();
 
-        //    Assert.Contains(daysString, holdingNew.TaxFreeStatus);
-        //    Assert.IsFalse(holdingNew.IsTaxFree);
-        //}
+            Assert.AreEqual(int.MaxValue, holding.TaxFreeDaysLeft);
+        }
+
+        [TestMethod]
+        public void TaxFreeDaysLeft_ReturnsZero_WhenHoldingIsTaxFree()
+        {
+            var holding = new MetalHolding
+            {
+                PurchaseDate = DateTime.Today.AddYears(-2)
+            };
+
+            Assert.AreEqual(0, holding.TaxFreeDaysLeft);
+        }
+
+        [TestMethod]
+        public void TaxFreeDaysLeft_ReturnsOne_OnDayBeforeTaxFree()
+        {
+            var holding = new MetalHolding
+            {
+                PurchaseDate = DateTime.Today.AddYears(-1).AddDays(1)
+            };
+
+            Assert.AreEqual(1, holding.TaxFreeDaysLeft);
+        }
+
+        [TestMethod]
+        public void TaxFreeDaysLeft_ReturnsRemainingDays_WhenHoldingIsNotTaxFree()
+        {
+            var purchaseDate = DateTime.Today.AddMonths(-6);
+            var expected = (purchaseDate.AddYears(1) - DateTime.Today).Days;
+
+            var holding = new MetalHolding
+            {
+                PurchaseDate = purchaseDate
+            };
+
+            Assert.AreEqual(expected, holding.TaxFreeDaysLeft);
+        }
 
         [TestMethod]
         public void TaxFreeSortValue_IsZero_ForTaxFree_OtherwiseDaysLeft()
@@ -138,6 +178,30 @@ namespace PreciousMetalsManager.Tests
 
             Assert.AreEqual(0, holdingOld.TaxFreeSortValue);
             Assert.AreEqual((almostOneYearAgo.AddYears(1) - now).Days, holdingNew.TaxFreeSortValue);
+        }
+
+        [TestMethod]
+        public void NotifyTaxFreeStatusChanged_RaisesAllRelatedProperties()
+        {
+            var holding = new MetalHolding();
+            var changedProperties = new List<string>();
+
+            holding.PropertyChanged += (_, e) =>
+            {
+                if (e.PropertyName is not null)
+                    changedProperties.Add(e.PropertyName);
+            };
+
+            holding.NotifyTaxFreeStatusChanged();
+
+            CollectionAssert.AreEquivalent(
+                new[]
+                {
+                    nameof(MetalHolding.IsTaxFree),
+                    nameof(MetalHolding.TaxFreeDaysLeft),
+                    nameof(MetalHolding.TaxFreeSortValue)
+                },
+                changedProperties);
         }
     }
 }
---
+++ tests/RelayCommandTest.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PreciousMetalsManager.Models;
+using System;
+using System.Windows.Input;
+using System.Windows.Threading;
+
+namespace PreciousMetalsManager.Tests
+{
+    [TestClass]
+    public class RelayCommandTest
+    {
+        private const string ExpectedParameter = "test-parameter";
+        private const int ExpectedPredicateParameter = 42;
+
+        [TestMethod]
+        public void Constructor_WithNullExecute_ThrowsArgumentNullException()
+        {
+            AssertThrows<ArgumentNullException>(() => _ = new RelayCommand(null!));
+        }
+
+        [TestMethod]
+        public void CanExecute_WithoutPredicate_ReturnsTrue()
+        {
+            var command = new RelayCommand(_ => { });
+
+            var result = command.CanExecute(null);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanExecute_WithPredicateReturningTrue_ReturnsTrue()
+        {
+            var command = new RelayCommand(_ => { }, _ => true);
+
+            var result = command.CanExecute(null);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanExecute_WithPredicateReturningFalse_ReturnsFalse()
+        {
+            var command = new RelayCommand(_ => { }, _ => false);
+
+            var result = command.CanExecute(null);
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void Execute_InvokesExecuteDelegate()
+        {
+            var wasCalled = false;
+            var command = new RelayCommand(_ => wasCalled = true);
+
+            command.Execute(null);
+
+            Assert.IsTrue(wasCalled);
+        }
+
+        [TestMethod]
+        public void Execute_PassesParameterToExecuteDelegate()
+        {
+            object? receivedParameter = null;
+            var command = new RelayCommand(parameter => receivedParameter = parameter);
+
+            command.Execute(ExpectedParameter);
+
+            Assert.AreEqual(ExpectedParameter, receivedParameter);
+        }
+
+        [TestMethod]
+        public void CanExecute_PassesParameterToPredicate()
+        {
+            object? receivedParameter = null;
+
+            var command = new RelayCommand(
+                _ => { },
+                parameter =>
+                {
+                    receivedParameter = parameter;
+                    return true;
+                });
+
+            _ = command.CanExecute(ExpectedPredicateParameter);
+
+            Assert.AreEqual(ExpectedPredicateParameter, receivedParameter);
+        }
+
+        [TestMethod]
+        public void CanExecuteChanged_WhenSubscribed_IsRaisedByCommandManagerRequerySuggested()
+        {
+            var command = new RelayCommand(_ => { });
+            var eventRaised = false;
+            EventHandler handler = (_, _) => eventRaised = true;
+
+            try
+            {
+                command.CanExecuteChanged += handler;
+
+                CommandManager.InvalidateRequerySuggested();
+                DrainDispatcher();
+
+                Assert.IsTrue(eventRaised);
+            }
+            finally
+            {
+                command.CanExecuteChanged -= handler;
+            }
+        }
+
+        private static void DrainDispatcher()
+        {
+            var frame = new DispatcherFrame();
+
+            Dispatcher.CurrentDispatcher.BeginInvoke(
+                DispatcherPriority.Background,
+                new Action(() => frame.Continue = false));
+
+            Dispatcher.PushFrame(frame);
+        }
+
+        private static void AssertThrows<TException>(Action action) where TException : Exception
+        {
+            try
+            {
+                action();
+                Assert.Fail($"Expected exception {typeof(TException).Name} was not thrown.");
+            }
+            catch (TException)
+            {
+                // expected
+            }
+        }
+    }
+}
